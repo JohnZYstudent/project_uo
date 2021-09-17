176d0ce0112b6bc81fda18cbe69fc0f241704598
Ziyuan Zhou <jzhou4@uoregon.edu>
2021-09-17 04:23:15 -0500

    large file

---
+++ HArena/Assets/Script/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private HealthSystem healthSystem;
+
+    public void Setup(HealthSystem healthSystem) {
+        this.healthSystem = healthSystem;
+    }
+    private void Changed(object sender, System.EventArgs e) {
+        transform.Find("Bar").localScale = new Vector3(healthSystem.GetPercentHealth()*10, 1);
+    }
+
+}
